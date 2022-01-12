5f10cd2ad29ceb57ec8966af8384a99fe9d08457
Matthias Feist <matze@matf.de>
2022-01-12 20:56:48 +0100

    first POC

---
+++ getSlStatus.js
@@ -0,0 +1,57 @@
+const axios = require('axios')
+
+module.exports = async function () {
+  const trafficSituation = await getTrafficsituationData()
+  const deviationsStatus = await getDeviationsData()
+
+  return {
+    train: trafficSituation.train && deviationsStatus.train,
+    subway: trafficSituation.subway && deviationsStatus.subway,
+  }
+}
+
+async function getTrafficsituationData() {
+  const response = await axios.get(
+    'https://api.sl.se/api2/trafficsituation.json?key=60efc7468e344f528b038b2781df025e'
+  )
+
+  let train = false
+  let subway = false
+  for (const trafficType of response.data?.ResponseData?.TrafficTypes) {
+    if (
+      trafficType.Type === 'metro' &&
+      trafficType.StatusIcon === 'EventGood'
+    ) {
+      subway = true
+    }
+
+    if (
+      trafficType.Type === 'train' &&
+      trafficType.StatusIcon === 'EventGood'
+    ) {
+      train = true
+    }
+  }
+
+  return { train, subway }
+}
+
+async function getDeviationsData() {
+  const response = await axios.get(
+    'https://api.sl.se/api2/deviationsrawdata.json?key=3c1b8eca287741069608162af0bdc6ea&transportMode=metro,train'
+  )
+
+  let train = true
+  let subway = true
+  for (const deviation of response.data?.ResponseData) {
+    if (deviation.TransportMode === 'METRO' && deviation.MainNews) {
+      subway = false
+    }
+
+    if (deviation.TransportMode === 'TRAIN' && deviation.MainNews) {
+      train = false
+    }
+  }
+
+  return { train, subway }
+}
--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-const { Blinkt, COLOURS } = require('blinkt-kit');
+const { Blinkt, COLOURS } = require('blinkt-kit')
+const getSlStatus = require('./getSlStatus.js')
 
-const blinkt = new Blinkt();
-blinkt.setAll({ r: 128, g: 0, b: 128, brightness: 0.2 });
-blinkt.show();
+const blinkt = new Blinkt()
+blinkt.setClearOnExit(true)
+
+// Set LED to a blue-ish color to indicate that it's loading...
+blinkt.setPixel({ pixel: 0, ...COLOURS.AQUA })
+blinkt.show()
+
+async function main() {
+  try {
+    const status = await getSlStatus()
+    showStatus('left', status.subway)
+    showStatus('right', status.train)
+  } catch {
+    blinkt.flashPixel({
+      pixel: 0,
+      times: 100,
+      intervalms: 100,
+      ...COLOURS.ORANGE,
+    })
+  }
+}
+main()
+
+function showStatus(side, status) {
+  const color = status ? COLOURS.LIME : COLOURS.RED
+  blinkt.clear()
+  if (side === 'left') {
+    console.log('left', color)
+    blinkt.setPixel({ pixel: 0, ...color })
+    blinkt.setPixel({ pixel: 1, ...color })
+    blinkt.setPixel({ pixel: 2, ...color })
+    blinkt.setPixel({ pixel: 3, ...color })
+  } else {
+    console.log('right', color)
+
+    blinkt.setPixel({ pixel: 4, ...color })
+    blinkt.setPixel({ pixel: 5, ...color })
+    blinkt.setPixel({ pixel: 6, ...color })
+    blinkt.setPixel({ pixel: 7, ...color })
+  }
+  blinkt.show()
+}
